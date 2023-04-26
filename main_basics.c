7870041bb2d2cfbb2dc62a3c896af4fdacb73817
charumotiramani <78335843+charumotiramani@users.noreply.github.com>
2023-04-26 11:28:49 +0530

    Add files via upload

---
+++ main_basics.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	char name[]="SYMBIOSIS";
+	printf("%s \n",name);
+	
+	int i=5;
+	printf("%d \n",i);
+	
+	float f=2.5;
+	printf("%f \n",f);
+	
+	int a;
+	scanf("%d",&a);
+	printf("%d",a);
+	
+	return 0;
+}
+
